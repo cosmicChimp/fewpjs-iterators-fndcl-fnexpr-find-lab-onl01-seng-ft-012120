e7685672337306a36be846928ed9ab094542fa5e
cosmicChimp <ian.t.marrs@gmail.com>
2020-05-04 11:14:55 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let result = array.find(e => e.result === "W")
+    if (result) {
+      return result.year;
+    } else {
+      return undefined
+    }
+      
+}
+
+superbowlWin(record);
